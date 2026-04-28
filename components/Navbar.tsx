8779bb3259f204dba19eb4f07592d14b73a7e2f3
Oscar Ramos <oscarramos@MacBook-Pro-de-Oscar.local>
2026-04-28 00:12:50 -0400

    blog + animaciones

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,17 +7,17 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 w-full z-50 bg-[#fdfcf8]/90 backdrop-blur border-b border-[#e0ddd6] px-6 md:px-10 h-[68px] flex items-center justify-between">
+    <nav className="fixed top-0 w-full z-50 bg-[#fdfcf8]/90 backdrop-blur border-b border-[#e0ddd6] px-6 md:px-10 h-[80px] flex items-center justify-between overflow-visible">
       
       {/* LOGO + NOMBRE */}
       <div className="flex items-center gap-3">
-        <Image
-          src="/logo.png"
-          alt="Logo Psicóloga"
-          width={48}
-height={48}
-className="object-contain opacity-90"
-        />
+       <Image
+  src="/logo.png"
+  alt="Logo"
+  width={72}
+  height={72}
+  className="object-contain -translate-y-1"
+/>
 
         <span className="text-[#4a6741] font-semibold font-serif text-base md:text-lg tracking-wide">
           Psicóloga Paulina Hernández
