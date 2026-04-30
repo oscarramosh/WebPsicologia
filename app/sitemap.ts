8894b2351e5943c70813e1a9e50c4d9f2aba62ae
Oscar Ramos <oscarramos@MacBook-Pro-de-Oscar.local>
2026-04-30 16:07:01 -0400

    blog + animaciones

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://larutadeunapsicologa.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://larutadeunapsicologa.com/agenda",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://larutadeunapsicologa.com/blog",
+      lastModified: new Date(),
+    },
+  ];
+}
